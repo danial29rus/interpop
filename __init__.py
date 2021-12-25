d1519d5ed46dc22ee797cc8bbc9f6ac9541850b5
Daniil <70702619+danial29rus@users.noreply.github.com>
2021-12-26 00:21:30 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: danial29rus,CHERNOV,martishenya,KAHOROV
+
+:Functions: 
+    interpop.interpol()
+    
+    
+    
+"""
+from .interpop import interpol
+__author__ = 'danial29rus'
+__version__ = '1.8'
+
